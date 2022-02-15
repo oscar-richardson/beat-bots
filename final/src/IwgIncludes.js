7efc445a8c21c28f8d62aea178972533defa156a
Oscar Richardson <oscarrichardson1999@gmail.com>
2022-02-15 19:08:42 +0000

    finished final MWE

---
+++ final/src/IwgIncludes.js
@@ -0,0 +1,112 @@
+var _manifest = [
+	//-----------------------------------------------------------------------------------------------
+	// 	Third party.
+	//-----------------------------------------------------------------------------------------------
+
+	// Essential.
+	{"src": "src/thirdParty/custom-pixi.js",								"id": "pixijs"},
+	{"src": "src/thirdParty/gsap.js",										"id": "gsap" },
+
+	// Non-essential?
+	{"src": "src/thirdParty/CustomBounce.min.js",							"id": "CustomBounce" },
+	{"src": "src/thirdParty/CustomEase.min.js",								"id": "CustomEase" },
+	{"src": "src/thirdParty/CustomWiggle.min.js",							"id": "CustomWiggle" },
+	{"src": "src/thirdParty/EasePack.min.js",								"id": "EasePack" },
+	{"src": "src/thirdParty/MotionPathHelper.min.js",						"id": "MotionPathHelper" },
+	{"src": "src/thirdParty/MotionPathPlugin.min.js",						"id": "MotionPathPlugin" },
+	{"src": "src/thirdParty/Physics2DPlugin.min.js",						"id": "Physics2DPlugin" },
+	{"src": "src/thirdParty/pixi-filters.min.js",							"id": "pixi-filters"},
+	{"src": "src/thirdParty/PixiPlugin.min.js",								"id": "PixiPlugin" },
+	{"src": "src/thirdParty/shockwave.min.js",								"id": "shockwavejs"},
+
+
+	//-----------------------------------------------------------------------------------------------
+	//	Graphics.
+	//-----------------------------------------------------------------------------------------------
+
+	{"src": "src/imports/img/BackgroundSS.png", 		"id": "BackgroundSS" },
+    {"src": "src/imports/js/BackgroundSS.js",  			"id": "Background" },
+
+	{"src": "src/imports/img/BB_LogoSS.png", 				"id": "BB_LogoSS" },
+    {"src": "src/imports/js/BB_LogoSS.js",  				"id": "BB_Logo" },
+
+    {"src": "src/imports/img/Robot1_AsleepSS.png", 			"id": "Robot1_AsleepSS" },
+    {"src": "src/imports/js/Robot1_AsleepSS.js",  			"id": "Robot1_Asleep" },
+    {"src": "src/imports/img/Robot1_DanceSS.png", 			"id": "Robot1_DanceSS" },
+    {"src": "src/imports/js/Robot1_DanceSS.js",  			"id": "Robot1_Dance" },
+    {"src": "src/imports/img/Robot1_WakeUpSS.png", 			"id": "Robot1_WakeUpSS" },
+    {"src": "src/imports/js/Robot1_WakeUpSS.js",  			"id": "Robot1_WakeUp" },
+
+    {"src": "src/imports/img/Robot2_AsleepSS.png", 			"id": "Robot2_AsleepSS" },
+    {"src": "src/imports/js/Robot2_AsleepSS.js",  			"id": "Robot2_Asleep" },
+    {"src": "src/imports/img/Robot2_DanceSS.png", 			"id": "Robot2_DanceSS" },
+    {"src": "src/imports/js/Robot2_DanceSS.js",  			"id": "Robot2_Dance" },
+    {"src": "src/imports/img/Robot2_WakeUpSS.png", 			"id": "Robot2_WakeUpSS" },
+    {"src": "src/imports/js/Robot2_WakeUpSS.js",  			"id": "Robot2_WakeUp" },
+
+    {"src": "src/imports/img/Robot3_AsleepSS.png", 			"id": "Robot3_AsleepSS" },
+    {"src": "src/imports/js/Robot3_AsleepSS.js",  			"id": "Robot3_Asleep" },
+    {"src": "src/imports/img/Robot3_DanceSS.png", 			"id": "Robot3_DanceSS" },
+    {"src": "src/imports/js/Robot3_DanceSS.js",  			"id": "Robot3_Dance" },
+    {"src": "src/imports/img/Robot3_WakeUpSS.png", 			"id": "Robot3_WakeUpSS" },
+    {"src": "src/imports/js/Robot3_WakeUpSS.js",  			"id": "Robot3_WakeUp" },
+
+    {"src": "src/imports/img/Robot4_AsleepSS.png", 			"id": "Robot4_AsleepSS" },
+    {"src": "src/imports/js/Robot4_AsleepSS.js",  			"id": "Robot4_Asleep" },
+    {"src": "src/imports/img/Robot4_DanceSS.png", 			"id": "Robot4_DanceSS" },
+    {"src": "src/imports/js/Robot4_DanceSS.js",  			"id": "Robot4_Dance" },
+    {"src": "src/imports/img/Robot4_WakeUpSS.png", 			"id": "Robot4_WakeUpSS" },
+    {"src": "src/imports/js/Robot4_WakeUpSS.js",  			"id": "Robot4_WakeUp" },
+
+    {"src": "src/imports/img/Robot5_AsleepSS.png", 			"id": "Robot5_AsleepSS" },
+    {"src": "src/imports/js/Robot5_AsleepSS.js",  			"id": "Robot5_Asleep" },
+    {"src": "src/imports/img/Robot5_DanceSS.png", 			"id": "Robot5_DanceSS" },
+    {"src": "src/imports/js/Robot5_DanceSS.js",  			"id": "Robot5_Dance" },
+    {"src": "src/imports/img/Robot5_WakeUpSS.png", 			"id": "Robot5_WakeUpSS" },
+    {"src": "src/imports/js/Robot5_WakeUpSS.js",  			"id": "Robot5_WakeUp" },
+
+    {"src": "src/imports/img/Robot6_AsleepSS.png", 			"id": "Robot6_AsleepSS" },
+    {"src": "src/imports/js/Robot6_AsleepSS.js",  			"id": "Robot6_Asleep" },
+    {"src": "src/imports/img/Robot6_DanceSS.png", 			"id": "Robot6_DanceSS" },
+    {"src": "src/imports/js/Robot6_DanceSS.js",  			"id": "Robot6_Dance" },
+    {"src": "src/imports/img/Robot6_WakeUpSS.png", 			"id": "Robot6_WakeUpSS" },
+    {"src": "src/imports/js/Robot6_WakeUpSS.js",  			"id": "Robot6_WakeUp" },
+
+
+	//-----------------------------------------------------------------------------------------------
+	//  BeabLib.
+	//-----------------------------------------------------------------------------------------------
+
+	{"src": "src/beablib/beablib_Camelot.js",	        					"id": "game_baseClass" },
+
+
+	//-----------------------------------------------------------------------------------------------
+	//	Game source.
+	//-----------------------------------------------------------------------------------------------
+
+	//	Base classes.
+	{"src": "src/game/CBackground.js", 					"id": "CBackground" },
+	{"src": "src/game/CGUI.js", 						"id": "CGUI" },
+	{"src": "src/game/CRobots.js", 					"id": "CRobots" },
+
+	//	Core game.
+	{"src": "src/game/CGame.js", 						"id": "CGame" }
+
+	//-----------------------------------------------------------------------------------------------
+];
+
+//-----------------------------------------------------------------------------------------------
+
+miwgdefine(function () {
+    "use strict";
+    //conditional statements can be added here
+    //com.camelot.core.IWG.ame('get', 'UA') is loaded and can be used.
+    /**
+     *   if ({your condition is met}) {
+     *   _manifest.unshift({"src": "pathToFile", "id": "fileID"})
+     *   }
+     */
+    return _manifest;
+});
+
+//-----------------------------------------------------------------------------------------------
---
+++ final/src/game/CGame.js
@@ -0,0 +1,132 @@
+//-----------------------------------------------------------------------------------------------
+
+(function() {
+    "use strict";
+
+    //-----------------------------------------------------------------------------------------------
+    //	Const-ish.
+    //-----------------------------------------------------------------------------------------------
+
+    //	Canvas sizes.
+	var	BACK_CANVAS_HEIGHT		=	768,
+    BACK_CANVAS_WIDTH		=	1136,
+    GAME_CANVAS_HEIGHT		=	768,
+    GAME_CANVAS_WIDTH		=	1136,
+    GUI_CANVAS_HEIGHT		=	768,
+    GUI_CANVAS_WIDTH		=	1136;
+
+    //-----------------------------------------------------------------------------------------------
+    //	Main variables.
+    //-----------------------------------------------------------------------------------------------
+
+    // Beablib alias.
+    var Game = beablib.Game,
+        Renderer = beablib.Renderer;
+
+    //	Game aliases.
+    var TheBackground = {},
+        TheRobots = {},
+        TheGUI = {};
+
+    //-----------------------------------------------------------------------------------------------
+
+    Game.FontDefinitions = [];
+
+    //-----------------------------------------------------------------------------------------------
+
+    Game.Init = function() {
+        beablib.log("Game::Init::Enter");
+
+        //	Configure beablib.
+        beablib.PauseEnabled = true;
+        beablib.ResizeEnabled = true;
+
+        //	Create the global sprite sheets.
+        Game.BackgroundSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.BackgroundSS);
+
+        Game.BBLogoSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.BB_LogoSS);
+
+        Game.Robot1AsleepSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot1_AsleepSS);
+        Game.Robot1DanceSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot1_DanceSS);
+        Game.Robot1WakeUpSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot1_WakeUpSS);
+
+        Game.Robot2AsleepSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot2_AsleepSS);
+        Game.Robot2DanceSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot2_DanceSS);
+        Game.Robot2WakeUpSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot2_WakeUpSS);
+
+        Game.Robot3AsleepSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot3_AsleepSS);
+        Game.Robot3DanceSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot3_DanceSS);
+        Game.Robot3WakeUpSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot3_WakeUpSS);
+
+        Game.Robot4AsleepSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot4_AsleepSS);
+        Game.Robot4DanceSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot4_DanceSS);
+        Game.Robot4WakeUpSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot4_WakeUpSS);
+
+        Game.Robot5AsleepSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot5_AsleepSS);
+        Game.Robot5DanceSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot5_DanceSS);
+        Game.Robot5WakeUpSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot5_WakeUpSS);
+
+        Game.Robot6AsleepSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot6_AsleepSS);
+        Game.Robot6DanceSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot6_DanceSS);
+        Game.Robot6WakeUpSheet = beablib.CreateSpriteSheet(beablib.SpriteSheetPath.Robot6_WakeUpSS);
+
+        gsap.registerPlugin(CustomBounce, CustomEase, CustomWiggle, MotionPathPlugin, PixiPlugin, Physics2DPlugin);
+
+        //	Create our game's canvases...
+        Game.GameCanvas = beablib.CreateCanvas({ id: "GameCanvas", zIndex: 4, position: "fixed", AddToParent: true });
+        Game.GUICanvas = beablib.CreateCanvas( {id:"GUICanvas", zIndex:10, position:"fixed", AddToParent:true });
+
+        //	...& stages.
+        Game.BackgroundStage = beablib.CreateStage(beablib.GetBaseCanvasName(), {width:BACK_CANVAS_WIDTH, height:BACK_CANVAS_HEIGHT, anchor_h:"centre", anchor_v:"centre", clamp:true});
+        Game.GameStage = beablib.CreateStage( "GameCanvas",	{width:GAME_CANVAS_WIDTH, height:GAME_CANVAS_HEIGHT, anchor_h:"centre", anchor_v:"centre"} );
+        Game.GUIStage = beablib.CreateStage( "GUICanvas",	{width:GUI_CANVAS_WIDTH, height:GUI_CANVAS_HEIGHT, anchor_h:"centre", anchor_v:"centre", clamp:true, touch:true} );
+
+        //	Create the game's components.
+        TheBackground = new Game.CBackground(Game.BackgroundStage);
+        TheRobots = new Game.CRobots( Game.GameStage );
+        TheGUI = new Game.CGUI( Game.GUIStage );
+
+        //	Set the tick handler going.
+        beablib.TweenHandler.Enable(true);
+        Game.GameCanvas.EnablePointerEvents(true);
+        // Game.GUICanvas.EnablePointerEvents( true );
+        // Game.GUIStage.nextStage		=	Game.GameStage;
+
+        beablib.log("Game::Init::Exit");
+    };
+
+    //-----------------------------------------------------------------------------------------------
+
+    Game.StartGame = function() {};
+
+    //-----------------------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------------------
+	//	Public.
+	//-----------------------------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------------------------
+
+    Game.RobotClicked				=	function( number )
+    {
+
+        TheRobots.RobotClicked( number );
+
+        console.log("Game.RobotClicked :: " + number );
+
+    };
+
+    //-----------------------------------------------------------------------------------------------
+
+    Game.InitRobots				=	function(  )
+    {
+
+        TheRobots.InitRobots(  );
+
+    };
+
+	//-----------------------------------------------------------------------------------------------
+
+}());
+
+//-----------------------------------------------------------------------------------------------
